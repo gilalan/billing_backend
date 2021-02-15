b48be6fe54e3f5eceb222ba498e0e96a4e400416
Gilliard Lopes <gilalan@gmail.com>
2021-02-14 22:40:57 -0300

    enabling cors via npm

--- src/config/server.js
+++ src/config/server.js
@@ -2,16 +2,23 @@ const PORT = process.env.PORT || 3003;
 
 //const bodyParser = require('body-parser');
 const express = require('express'); //singleton, posso declarar em outro lugar que vai ser a mesma instância
-
-const allowCors = require('./cors');
+const cors = require('cors');
+//const allowCors = require('./cors');
+//server.use(allowCors);
 
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
-//server.use(bodyParser.urlencoded({ extended: true }));
-//server.use(bodyParser.json());
-server.use(allowCors);
+server.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    server.use(cors());
+    next();
+});
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}...`));
 
